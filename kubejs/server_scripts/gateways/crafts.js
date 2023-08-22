e451a94260c495cb999b0ac8924131d0d1082950
MLDEG <mrmldeg@gmail.com>
2023-08-23 01:50:41 +0200

    feat: add craft for all new gateways

--- kubejs/server_scripts/gateways/crafts.js
+++ kubejs/server_scripts/gateways/crafts.js
@@ -9,4 +9,27 @@ onEvent('recipes', e => {
     //Large
     e.custom({"type":"createaddition:charging","input": {"item": "gateways:catalyst_medium_pearl_gate","count": 1},"result": {"item": "gateways:catalyst_large_pearl_gate","count": 1},"energy": 100000})
 
+    function gate(item, mob){[e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": [" # ", "#A#", " # "],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_small_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate_small"}),e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": ["###", "#A#", "###"],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_medium_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate"}),e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": ["###", "#A#", "###"],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_large_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate_large"})]}
+
+    gate('twilightforest:arctic_fur', 'yeti')
+    gate('bhc:wither_bone', 'wither')
+    gate('thermal:blitz_rod', 'blitz')
+    gate('thermal:blizz_rod', 'blizz')
+    gate('tconstruct:ender_slime_ball', 'ender_slime')
+    gate('tconstruct:sky_slime_ball', 'sky_slime')
+    gate('minecraft:clay_ball', 'terracube')
+    gate('twilightforest:torchberries', 'skeleton_druid')
+    gate('minecraft:book', 'death_tome')
+    gate('undergarden:forgotten_nugget', 'forgotten_guardian')
+    gate('minecraft:diamond', 'star_crawler')
+    gate('ars_nouveau:wilden_spike', 'wilden_guardian')
+    gate('ars_nouveau:wilden_horn', 'wilden_hunter')
+    gate('ars_nouveau:wilden_wing', 'wilden_stalker')
+    gate('minecraft:phantom_membrane', 'phantom')
+    gate('botania:pixie_dust', 'alf_pixie')
+    gate('twilightforest:armor_shard', 'blockchain_goblin')
+    gate('minecraft:coal', 'foxhound')
+    gate('twilightforest:borer_essence', 'towerwood_borer')
+    gate('twilightforest:raw_meef', 'minotaur')
+
 })
