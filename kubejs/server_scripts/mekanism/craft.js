8961f111fbebc4b66382439d99d81ce4cc6dcc3a
MLDEG <mrmldeg@gmail.com>
2023-09-05 19:33:55 +0200

    add alternative craft for machine frame

---
+++ kubejs/server_scripts/mekanism/craft.js
@@ -0,0 +1,10 @@
+onEvent('recipes', e => {
+
+e.custom({
+  "type": "mekanism:combining",
+  "mainInput": { "amount": 1, "ingredient": { "item": "industrialforegoing:machine_frame_supreme" } },
+  "extraInput": { "ingredient": { "item": "mekanism:block_steel" } },
+  "output": { "item": "mekanism:steel_casing" }
+})
+
+})
---
+++ kubejs/server_scripts/thermal/craft.js
@@ -0,0 +1,15 @@
+onEvent('recipes', e => {
+
+e.custom({
+  "type": "thermal:smelter",
+  "input": [
+    { "item": "immersiveengineering:sheetmetal_iron" },
+    { "item": "immersiveengineering:sheetmetal_steel" },
+    { "item": "industrialforegoing:machine_frame_simple"}
+  ],
+  "result": [{ "item": "thermal:machine_frame", "count": 1 }],
+  "energy": 3200
+}
+)
+
+})
