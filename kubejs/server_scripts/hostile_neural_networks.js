795dd48371b36c60328d09ea7b5d63cea6b072cd
MLDEG <mrmldeg@gmail.com>
2023-09-28 03:43:27 +0200

    add craft for the undergarden prediction

---
+++ kubejs/server_scripts/hostile_neural_networks.js
@@ -0,0 +1,11 @@
+onEvent('recipes', e => {
+
+  e.shapeless('undergarden:gronglet', ['hostilenetworks:undergarden_prediction', '3x twilightforest:moonworm'])
+  e.shapeless('3x undergarden:twistytwig', ['hostilenetworks:undergarden_prediction', '3x minecraft:string'])
+  e.shapeless('3x undergarden:ditchbulb', ['hostilenetworks:undergarden_prediction', '3x minecraft:coal'])
+  e.shapeless('3x undergarden:gloomgourd', ['hostilenetworks:undergarden_prediction', '3x minecraft:pumpkin'])
+  e.shapeless('3x undergarden:glitterkelp', ['hostilenetworks:undergarden_prediction', '3x minecraft:kelp'])
+  e.shapeless('3x minecraft:cocoa_beans', ['hostilenetworks:undergarden_prediction', '3x undergarden:underbeans'])
+  e.shapeless('3x undergarden:droopvine_item', ['hostilenetworks:undergarden_prediction', '3x minecraft:sweet_berries'])
+
+})
