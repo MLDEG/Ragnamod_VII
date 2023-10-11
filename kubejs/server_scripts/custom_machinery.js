7606dd6231f65b258c3c056e1cb27c198a173b40
MLDEG <mrmldeg@gmail.com>
2023-10-11 19:29:08 +0200

    Blood Magic Quests DONE

---
+++ kubejs/server_scripts/custom_machinery.js
@@ -0,0 +1,5 @@
+onEvent('recipes', e => {
+
+  e.shaped(Item.of('custommachinery:custom_machine_item', '{machine:"ragnamod_7:will_factory"}'), ['ABA','BCD', 'ABA'], {A: 'ftbic:iridium_circuit', B: 'bloodmagic:ritualstone', C: 'xnet:controller', D: 'rftoolsutility:screen'})
+
+})
