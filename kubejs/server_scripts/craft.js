05c3270a7ec5571c3a909c25eeb722ce79c2c13b
MLDEG <mrmldeg@gmail.com>
2023-11-11 17:31:27 +0100

    rename singularity catalyst to silent

--- kubejs/server_scripts/craft.js
+++ kubejs/server_scripts/craft.js
@@ -3,7 +3,6 @@ onEvent('recipes', e => {
   //Silicon Block
   e.shapeless('9x ftbic:silicon', 'ftbic:silicon_block')
   e.shapeless('ftbic:silicon_block', '9x ftbic:silicon')
-
   //Cataclysm
   //Ignitium
   e.shapeless('9x cataclysm:ignitium_ingot', 'cataclysm:ignitium_block')
@@ -40,7 +39,6 @@ onEvent('recipes', e => {
   //Bright Opal
   e.blasting('gemsnjewels:opal', 'gemsnjewels:bright_opal_ore_block')
   e.smelting('gemsnjewels:opal', 'gemsnjewels:bright_opal_ore_block')
-
   /***************************Horsmen Stuff***************************/
   /***************************Death***************************/
   e.shapeless('9x ragnamod_seven:death_nugget', 'ragnamod_seven:death_ingot')
@@ -116,7 +114,6 @@ onEvent('recipes', e => {
   e.custom({ "type": "create:compacting", "ingredients": [{ "item": "ragnamod_seven:famine_essence" }, { "item": "thermal:netherite_dust" }], "results": [{ "item": "ragnamod_seven:famine_essence" }, { "item": "ragnamod_seven:famine_essence_materialized" }], "heatRequirement": "superheated" })
   //Ingot
   e.custom({ "type": "evilcraft:blood_infuser", "item": "ragnamod_seven:famine_essence_materialized", "fluid": { "fluid": "evilcraft:blood", "amount": 100000 }, "result": { "item": "ragnamod_seven:famine_ingot" }, "duration": 1200, "xp": 20, "tier": 3 })
-
   //Corundum Block
   //RED
   e.shapeless('quark:red_corundum', '4x quark:red_corundum_cluster')
@@ -145,173 +142,89 @@ onEvent('recipes', e => {
   //BLACK
   e.shapeless('quark:black_corundum', '4x quark:black_corundum_cluster')
   e.shapeless('4x quark:black_corundum_cluster', 'quark:black_corundum')
-
-
   //Nether Star Shard
   e.shapeless('minecraft:nether_star', '9x chickens:nether_star_shard')
   e.shapeless('9x chickens:nether_star_shard', 'minecraft:nether_star')
-
   //HOP Graphite Block
   e.shapeless('immersiveengineering:storage_hop_graphite', '9x immersiveengineering:ingot_hop_graphite')
   e.shapeless('9x immersiveengineering:ingot_hop_graphite', 'immersiveengineering:storage_hop_graphite')
-
   //Black Lotus
-  e.shaped('botania:black_lotus', [
-    'AAA',
-    'ASA',
-    'AAA'
-  ], {
+  e.shaped('botania:black_lotus', ['AAA', 'ASA', 'AAA'], {
     S: 'psi:cad_colorizer_purple',
     A: 'psi:ebony_substance'
   })
-
   //Void Stone
-  e.shaped('8x cataclysm:void_stone', [
-    'AAA',
-    'ASA',
-    'AAA'
-  ], {
+  e.shaped('8x cataclysm:void_stone', ['AAA', 'ASA', 'AAA'], {
     S: 'bloodmagic:reagentvoid',
     A: 'minecraft:redstone_block'
   })
-
   //Ender Stone
-  e.shaped('8x cataclysm:ender_stone', [
-    'AAA',
-    'ASA',
-    'AAA'
-  ], {
+  e.shaped('8x cataclysm:ender_stone', ['AAA', 'ASA', 'AAA'], {
     S: 'botania:ender_air_bottle',
     A: 'cataclysm:void_stone'
   })
-
   //Oak Log
-  e.shaped('16x minecraft:oak_log', [
-    'AAA',
-    '   ',
-    '   '
-  ], {
+  e.shaped('16x minecraft:oak_log', ['AAA', '   ', '   '], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Spruce Log
-  e.shaped('16x minecraft:spruce_log', [
-    '  A',
-    ' A ',
-    'A  '
-  ], {
+  e.shaped('16x minecraft:spruce_log', ['  A', ' A ', 'A  '], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Birch Log
-  e.shaped('16x minecraft:birch_log', [
-    'A  ',
-    ' A ',
-    '  A'
-  ], {
+  e.shaped('16x minecraft:birch_log', ['A  ', ' A ', '  A'], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Jungle Log
-  e.shaped('16x minecraft:jungle_log', [
-    ' A ',
-    ' A ',
-    ' A '
-  ], {
+  e.shaped('16x minecraft:jungle_log', [' A ', ' A ', ' A '], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Acacia Log
-  e.shaped('16x minecraft:acacia_log', [
-    ' A ',
-    'A  ',
-    ' A '
-  ], {
+  e.shaped('16x minecraft:acacia_log', [' A ', 'A  ', ' A '], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Dark Oak Log
-  e.shaped('16x minecraft:dark_oak_log', [
-    ' A ',
-    '  A',
-    ' A '
-  ], {
+  e.shaped('16x minecraft:dark_oak_log', [' A ', '  A', ' A '], {
     A: 'ragnamod_seven:log_egg_component'
   })
-
   //Crimson Leg
-  e.shaped('16x minecraft:crimson_stem', [
-    'NA ',
-    'AA ',
-    '   '
-  ], {
+  e.shaped('16x minecraft:crimson_stem', ['NA ', 'AA ', '   '], {
     A: 'ragnamod_seven:log_egg_component',
     N: 'minecraft:netherrack'
   })
-
   //Warped Leg
-  e.shaped('16x minecraft:warped_stem', [
-    'AN ',
-    'AA ',
-    '   '
-  ], {
+  e.shaped('16x minecraft:warped_stem', ['AN ', 'AA ', '   '], {
     A: 'ragnamod_seven:log_egg_component',
     N: 'minecraft:netherrack'
   })
-
   //Summoning Table
-  e.shaped('summoningrituals:altar', [
-    'ABA',
-    'CDC',
-    'DED'
-  ], {
+  e.shaped('summoningrituals:altar', ['ABA', 'CDC', 'DED'], {
     A: 'minecraft:candle',
     B: 'minecraft:wither_skeleton_skull',
     C: 'minecraft:gold_block',
     D: 'minecraft:red_carpet',
     E: 'immersiveengineering:craftingtable'
   })
-
   //Generalized Twilight Prediction
-  e.shaped('hostilenetworks:twilight_prediction', [
-    ' A ',
-    'ABA',
-    ' A '
-  ], {
+  e.shaped('hostilenetworks:twilight_prediction', [' A ', 'ABA', ' A '], {
     A: 'hostilenetworks:end_prediction',
     B: 'twilightforest:root'
   })
-
   //Generalized Undergarden Prediction
-  e.shaped('hostilenetworks:undergarden_prediction', [
-    ' A ',
-    'ABA',
-    ' A '
-  ], {
+  e.shaped('hostilenetworks:undergarden_prediction', [' A ', 'ABA', ' A '], {
     A: 'hostilenetworks:twilight_prediction',
     B: 'undergarden:depthrock'
   })
-
   //Generalized Blueskies Prediction
-  e.shaped('hostilenetworks:blueskies_prediction', [
-    ' A ',
-    'ABA',
-    ' A '
-  ], {
+  e.shaped('hostilenetworks:blueskies_prediction', [' A ', 'ABA', ' A '], {
     A: 'hostilenetworks:undergarden_prediction',
     B: 'blue_skies:turquoise_stone'
   })
-
   //Ghast Cow Star
-  e.shaped('ragnamod_seven:ghast_cow_star', [
-    ' A ',
-    'ABA',
-    ' A '
-  ], {
+  e.shaped('ragnamod_seven:ghast_cow_star', [' A ', 'ABA', ' A '], {
     A: 'ragnamod_seven:ghast_cow_tear',
     B: 'forbidden_arcanus:dark_nether_star'
   })
-
   //Blood Magic Ritual Stone
   function ritualStone(input, output, catalyst) { e.custom({ "type": "create:item_application", "ingredients": [{ "item": input }, { "item": catalyst }], "results": [{ "item": output }] }) }
   ritualStone('bloodmagic:ritualstone', 'bloodmagic:airritualstone', 'bloodmagic:airscribetool')
@@ -319,16 +232,11 @@ onEvent('recipes', e => {
   ritualStone('bloodmagic:ritualstone', 'bloodmagic:fireritualstone', 'bloodmagic:waterscribetool')
   ritualStone('bloodmagic:ritualstone', 'bloodmagic:earthritualstone', 'bloodmagic:earthscribetool')
   ritualStone('bloodmagic:ritualstone', 'bloodmagic:duskritualstone', 'bloodmagic:duskscribetool')
-
   //Chemical Core
   e.custom({
     "type": "draconicevolution:fusion_crafting",
-    "result": {
-      "item": "ragnamod_seven:chemical_core"
-    },
-    "catalyst": {
-      "item": "draconicevolution:chaotic_core"
-    },
+    "result": { "item": "ragnamod_seven:chemical_core" },
+    "catalyst": { "item": "draconicevolution:chaotic_core" },
     "total_energy": 5120000000,
     "tier": "CHAOTIC",
     "ingredients": [
@@ -346,21 +254,13 @@ onEvent('recipes', e => {
       { "item": "chemlib:francium_metal_block" }
     ]
   })
-
   //Chemical Draconium Block
   e.custom({
     "type": "draconicevolution:fusion_crafting",
-    "result": {
-      "item": "ragnamod_seven:chemical_draconium_block",
-      "count": 4
-    },
+    "result": { "item": "ragnamod_seven:chemical_draconium_block", "count": 4 },
     "catalyst": {
       "count": 4,
-      "items": [
-        {
-          "item": "draconicevolution:awakened_draconium_block"
-        }
-      ],
+      "items": [{ "item": "draconicevolution:awakened_draconium_block" }],
       "type": "draconicevolution:ingredient_stack"
     },
     "total_energy": 10240000000,
@@ -374,6 +274,13 @@ onEvent('recipes', e => {
       { "item": "ragnamod_seven:chemical_core" },
       { "item": "ragnamod_seven:chemical_core" }
     ]
+  })
+  //Singularity Silent
+  e.custom({
+    "type": "architects_palette:warping",
+    "ingredient": [{ "item": "warden_and_sculk:sculk_shrieker" }],
+    "result": { "item": "ragnamod_seven:singularity_silent" },
+    "dimension": "minecraft:the_nether"
   })
 
 })
