8e5455acf737f6e5da07ad2ce4046b7d433f5e35
MLDEG <mrmldeg@gmail.com>
2023-11-29 15:16:46 +0100

    add basalz craft for gateways

--- kubejs/server_scripts/gateways/crafts.js
+++ kubejs/server_scripts/gateways/crafts.js
@@ -1,20 +1,21 @@
 onEvent('recipes', e => {
 
     //Small Gate Catalyst
-    e.shaped('gateways:catalyst_small_pearl_gate', [' A ','ASA',' A '], {A: 'bloodmagic:reagentvoid',S: 'ae2:fluix_pearl'})
-    
-    //Medium
-    e.custom({"type": "botania:runic_altar","output": {"item": "gateways:catalyst_medium_pearl_gate"},"mana": 10000,"ingredients": [{"item": "gateways:catalyst_small_pearl_gate"},{"item": "gateways:catalyst_small_pearl_gate"},{"item": "gateways:catalyst_small_pearl_gate"},{"item": "gateways:catalyst_small_pearl_gate"},{"item": "botania:pixie_dust"}]})
-    
-    //Large
-    e.custom({"type":"createaddition:charging","input": {"item": "gateways:catalyst_medium_pearl_gate","count": 1},"result": {"item": "gateways:catalyst_large_pearl_gate","count": 1},"energy": 100000})
+    e.shaped('gateways:catalyst_small_pearl_gate', [' A ', 'ASA', ' A '], { A: 'bloodmagic:reagentvoid', S: 'ae2:fluix_pearl' })
 
-    function gate(item, mob){[e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": [" # ", "#A#", " # "],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_small_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate_small"}),e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": ["###", "#A#", "###"],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_medium_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate"}),e.custom({"type": "gateways:gate_recipe","group": "gateways","pattern": ["###", "#A#", "###"],"key": {"#": {"item": item},"A": {"item": "gateways:catalyst_large_pearl_gate"}},"result": {"item": "gateways:gate_pearl"},"gateway": "gateways:"+mob+"_gate_large"})]}
+    //Medium
+    e.custom({ "type": "botania:runic_altar", "output": { "item": "gateways:catalyst_medium_pearl_gate" }, "mana": 10000, "ingredients": [{ "item": "gateways:catalyst_small_pearl_gate" }, { "item": "gateways:catalyst_small_pearl_gate" }, { "item": "gateways:catalyst_small_pearl_gate" }, { "item": "gateways:catalyst_small_pearl_gate" }, { "item": "botania:pixie_dust" }] })
+
+    //Large
+    e.custom({ "type": "createaddition:charging", "input": { "item": "gateways:catalyst_medium_pearl_gate", "count": 1 }, "result": { "item": "gateways:catalyst_large_pearl_gate", "count": 1 }, "energy": 100000 })
+
+    function gate(item, mob) { [e.custom({ "type": "gateways:gate_recipe", "group": "gateways", "pattern": [" # ", "#A#", " # "], "key": { "#": { "item": item }, "A": { "item": "gateways:catalyst_small_pearl_gate" } }, "result": { "item": "gateways:gate_pearl" }, "gateway": "gateways:" + mob + "_gate_small" }), e.custom({ "type": "gateways:gate_recipe", "group": "gateways", "pattern": ["###", "#A#", "###"], "key": { "#": { "item": item }, "A": { "item": "gateways:catalyst_medium_pearl_gate" } }, "result": { "item": "gateways:gate_pearl" }, "gateway": "gateways:" + mob + "_gate" }), e.custom({ "type": "gateways:gate_recipe", "group": "gateways", "pattern": ["###", "#A#", "###"], "key": { "#": { "item": item }, "A": { "item": "gateways:catalyst_large_pearl_gate" } }, "result": { "item": "gateways:gate_pearl" }, "gateway": "gateways:" + mob + "_gate_large" })] }
 
     gate('twilightforest:arctic_fur', 'yeti')
     gate('bhc:wither_bone', 'wither')
     gate('thermal:blitz_rod', 'blitz')
     gate('thermal:blizz_rod', 'blizz')
+    gate('thermal:basalz_rod', 'basalz')
     gate('tconstruct:ender_slime_ball', 'ender_slime')
     gate('tconstruct:sky_slime_ball', 'sky_slime')
     gate('minecraft:clay_ball', 'terracube')
