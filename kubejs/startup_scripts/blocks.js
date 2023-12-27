548f2e4d7c607462e4cc9e6aabeaf8608a099f01
MLDEG <mrmldeg@gmail.com>
2023-12-27 14:56:53 +0100

    add Castle Block

--- kubejs/startup_scripts/blocks.js
+++ kubejs/startup_scripts/blocks.js
@@ -19,6 +19,7 @@ onEvent('block.registry', e => {
     e.create('ragnamod_seven:singularity_silent').displayName('Singularity silent').material('portal').hardness(5.0).tagBlock('minecraft:mineable/pickaxe').notSolid().renderType('cutout').defaultTranslucent().redstoneConductor(false).waterlogged()
     e.create('ragnamod_seven:ae2').displayName('Controller Online').material('stone')
     e.create('ragnamod_seven:psi_stabilizer').displayName('PSI Stabilizer').material('stone').hardness(5.0).tagBlock('minecraft:mineable/pickaxe').notSolid().renderType('cutout').defaultTranslucent().redstoneConductor(false).waterlogged()
+    e.create('ragnamod_seven:castle_block').displayName('Castle Block').material('stone').hardness(5.0).tagBlock('minecraft:mineable/pickaxe')
 
 
 })
