25b865907bf74778a4b74c7c5cb3d961e8712294
MLDEG <mrmldeg@gmail.com>
2024-01-04 04:50:30 +0100

    add a solid nuclear waste

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -86,4 +86,5 @@ onEvent('item.registry', e => {
     e.create('ragnamod_seven:holy_powders').displayName('Holy Magic Powders')
     e.create('ragnamod_seven:creative_tc_crystal').displayName("Creative Tinkers' Crystal").glow(true).maxStackSize(1)
     e.create('ragnamod_seven:alloy_matrix').displayName("§cMatrix Alloy").glow(true).maxStackSize(1)
+    e.create('ragnamod_seven:solid_nuclear_waste').displayName("Solid Nuclear Waste")
 })
