f9dc16332c4ff0a696d1a9ee2f74d91a94af1ba8
MLDEG <mrmldeg@gmail.com>
2024-01-18 19:14:48 +0100

    Add Ichor fluid

--- kubejs/startup_scripts/fluids.js
+++ kubejs/startup_scripts/fluids.js
@@ -3,4 +3,10 @@ onEvent('fluid.registry', e => {
         .thinTexture(0xa19fc9)
         .bucketColor(0xa19fc9)
         .displayName('Aureal')
+
+    e.create('ichor')
+        .displayName('Ichor')
+        .stillTexture('tconstruct:ichor_still')
+        .flowingTexture('tconstruct:ichor_flowing')
+        .bucketColor(0xcc4100)
 })
