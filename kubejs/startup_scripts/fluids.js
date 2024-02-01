7b4fc4532baaec336680fa1161bf14f46bc2ae2f
MLDEG <mrmldeg@gmail.com>
2024-02-01 19:54:47 +0100

    Move Ichor fluid in a texture fluid folder + add Chaos Liquid

--- kubejs/startup_scripts/fluids.js
+++ kubejs/startup_scripts/fluids.js
@@ -6,7 +6,13 @@ onEvent('fluid.registry', e => {
 
     e.create('ichor')
         .displayName('Ichor')
-        .stillTexture('tconstruct:ichor_still')
-        .flowingTexture('tconstruct:ichor_flowing')
+        .stillTexture('tconstruct:fluid/ichor_still')
+        .flowingTexture('tconstruct:fluid/ichor_flowing')
         .bucketColor(0xcc4100)
+
+    e.create('chaos_liquid')
+        .displayName('Chaos Liquid')
+        .stillTexture('ragnamod_seven:fluid/chaos_still')
+        .flowingTexture('ragnamod_seven:fluid/chaos_flowing')
+        .bucketColor(0x0d0d0d)
 })
